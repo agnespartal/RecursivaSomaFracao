16b412874eaf10c2a19f3d1abe6c53aec74bcd00
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-21 19:12:36 -0300

    Update FracaoController.java

--- Exec3/src/Controller/FracaoController.java
+++ Exec3/src/Controller/FracaoController.java
@@ -8,11 +8,13 @@ public class FracaoController {
 
 	public double fracaoSoma (int num) {
 		double soma = 0;
+		//condição de parada quando valor inicial chegar em 1
 		if (num == 1) {
 			return 1;
 		} else {
 			soma = soma + (1/(double)num);
-			return soma + fracaoSoma(num -1) ;
+			return soma + fracaoSoma(num -1);
+			// soma da fração com o valor inicial decaindo a cada chamada até o 1
 		}
 	}
 }
